18e61e98e24fc8e88c4008d816a2955fa93a768d
Dong-Yeop Shin <stephano.dyshin@gmail.com>
2025-04-01 15:10:59 +0900

    upto 50

---
+++ 50_closest.py
@@ -0,0 +1,25 @@
+def find_previous_occurrence(index, s):
+    # loop B
+    print(f'index: {index}, s: {s}')
+    for j in range(index - 1, -1, -1):
+        # go throuigh the string from the current index to the start
+        print(f's: {s}, j: {j}, index: {index}')
+        if s[index] == s[j]:
+            # if the character at eht current index is equal
+            # to the character at index j
+            print(f's: {s}, j: {j}, index: {index}, index - j: {index - j}')
+            return index - j
+            # return the difference between the current index j
+    return -1
+
+def solution(s):
+    result = []
+    # loop A
+    for index in range(len(s)):
+        # for 'banana', the first index is 0, the second is 1, and so on
+        result.append(find_previous_occurrence(index, s))
+        print(f'result: {result}')
+        
+    return result
+
+print(solution("banana"))
